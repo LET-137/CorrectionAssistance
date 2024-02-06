f20dee1832968bfe7856314ce2b951bf691de89f
LET <bjs.6300.137@gmail.com>
2024-02-06 10:39:01 +0900

    Initial Commit

---
+++ CorrectionAssistance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CorrectionAssistance
+//
+//  Created by 津本拓也 on 2024/02/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CorrectionAssistance/CorrectionAssistanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CorrectionAssistanceApp.swift
+//  CorrectionAssistance
+//
+//  Created by 津本拓也 on 2024/02/06.
+//
+
+import SwiftUI
+
+@main
+struct CorrectionAssistanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
